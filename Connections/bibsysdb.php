f335c87c04a585f0e09989e3463edaea4550b60e
Fernando Shinohara <fernandots1996@hotmail.com>
2019-05-15 14:41:30 -0300

    Update bibsysdb.php

--- Connections/bibsysdb.php
+++ Connections/bibsysdb.php
@@ -1,7 +1,7 @@
 <?php
 $hostname_bibsysdb = "localhost";
 $database_bibsysdb = "bibsystem";
-$username_bibsysdb = "nandotets";
-$password_bibsysdb = "mano123";
+$username_bibsysdb = "usern";
+$password_bibsysdb = "passw";
 $bibsysdb = mysqli_connect($hostname_bibsysdb, $username_bibsysdb, $password_bibsysdb, $database_bibsysdb); 
 ?>
